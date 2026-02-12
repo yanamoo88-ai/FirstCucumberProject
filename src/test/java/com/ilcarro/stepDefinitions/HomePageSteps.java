e25050f9de8d268f358670fb61da8883719ac9a4
yanam <yanamoo88@gmail.com>
2026-02-12 12:49:38 +0100

    add

---
+++ src/test/java/com/ilcarro/stepDefinitions/HomePageSteps.java
@@ -0,0 +1,29 @@
+package com.ilcarro.stepDefinitions;
+
+import com.ilcarro.pages.HomePage;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import static com.ilcarro.pages.BasePage.driver;
+
+public class HomePageSteps {
+
+    HomePage home;
+
+    @Given("User launches Chrome browser")
+    public void launch_Chrome_browser(){
+        home = new HomePage(driver);
+        home.launchBrowser();
+    }
+
+    @When("User opens ilcarro HomePage")
+    public void open_ilcarro_HomePage(){
+        home.openUrl();
+    }
+
+    @Then("User verifies HomePage title is displayed")
+    public void verify_HomePage_title(){
+        new HomePage(driver).isHomePageTitleDisplayed();
+    }
+}
