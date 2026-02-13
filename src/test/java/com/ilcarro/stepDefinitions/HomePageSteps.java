c78ee3150d4be54529ebe4745ccc06bed3a77e91
yanam <yanamoo88@gmail.com>
2026-02-13 11:40:01 +0100

    add

--- src/test/java/com/ilcarro/stepDefinitions/HomePageSteps.java
+++ src/test/java/com/ilcarro/stepDefinitions/HomePageSteps.java
@@ -1,6 +1,7 @@
 package com.ilcarro.stepDefinitions;
 
 import com.ilcarro.pages.HomePage;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -25,5 +26,10 @@ public class HomePageSteps {
     @Then("User verifies HomePage title is displayed")
     public void verify_HomePage_title(){
         new HomePage(driver).isHomePageTitleDisplayed();
+    }
+
+    @And("User closes browser")
+    public void quit_browser(){
+        new HomePage(driver).quitBrowser();
     }
 }
